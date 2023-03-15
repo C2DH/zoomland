4635c78102f62ee4901be2985d9a79f87dd6218a
Daniele Guido <gui.daniele@gmail.com>
2023-03-15 09:23:15 +0100

    next man

--- src/compontents/Man.js
+++ src/compontents/Man.js
@@ -1,34 +1,171 @@
 import React, { useEffect, useRef } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
+import { useCharacterAnimations } from '../contexts/CharacterAnimations'
 
-export function Man(props) {
+const Man = (props) => {
   const group = useRef()
-  const { nodes, materials, animations } = useGLTF('./models/Man.glb')
+  const { nodes, animations } = useGLTF('./models//Man.gltf')
+  const { setAnimations, animationIndex } = useCharacterAnimations()
   const { actions, names } = useAnimations(animations, group)
 
-  console.log(names)
-  // useEffect(() => {
-  //   actions[names[1]].play()
-  // }, [])
+  useEffect(() => {
+    setAnimations(names)
+  }, [names])
+
+  useEffect(() => {
+    console.debug('actions: ', actions)
+    actions[names[7]].reset().fadeIn(0.5).play()
+    return () => {
+      actions[names[7]].fadeOut(0.5)
+    }
+  }, [animationIndex])
 
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
-        <mesh
-          name="Man"
-          castShadow
-          receiveShadow
-          geometry={nodes.Man.geometry}
-          material={nodes.Man.material}
-          position={[0, -0.3, 0]}
-          rotation={[0, 0, 0]}
-          scale={0.4}
+        <group name="cs_arm_fk" position={[1.5, 8.5, 0]} scale={0.82} />
+        <group name="cs_calf_fk" position={[0.5, 8.5, 0]} scale={0.82} />
+        <group name="cs_circle" position={[0.5, 4.5, 0]} scale={0.21} />
+        <group name="cs_circle001" position={[0.5, 4.5, 0]} scale={0.21} />
+        <group name="cs_circle_025" position={[2.5, 4.5, 0]} scale={0.21} />
+        <group name="cs_foot" position={[0.5, 10.5, 0]} rotation={[-Math.PI, 0, 0]} scale={0.31} />
+        <group
+          name="cs_foot001"
+          position={[0.5, 10.5, 0]}
+          rotation={[-Math.PI, 0, 0]}
+          scale={0.31}
         />
+        <group
+          name="cs_foot002"
+          position={[0.5, 10.5, 0]}
+          rotation={[-Math.PI, 0, 0]}
+          scale={0.31}
+        />
+        <group name="cs_foot_01" position={[0.5, 18.5, 0]} rotation={[0, 1.57, 0]} scale={2.19} />
+        <group name="cs_foot_roll" position={[0.5, 12.5, 0]} scale={0.59} />
+        <group name="cs_forearm_fk" position={[2.5, 8.5, 0]} scale={0.82} />
+        <group name="cs_hand" position={[0.5, 19.5, 0]} rotation={[-Math.PI, 0, 0]} scale={0.31} />
+        <group name="cs_head" position={[0.5, 13.5, 0]} scale={0.21} />
+        <group name="cs_hips" position={[0.5, 11.5, 0]} scale={0.21} />
+        <group name="cs_master" position={[0.5, 17.5, 0]} scale={0.1} />
+        <group name="cs_neck" position={[0.5, 14.5, 0]} scale={0.21} />
+        <group
+          name="cs_shoulder_left"
+          position={[0.5, 15.5, 0]}
+          rotation={[-Math.PI, -Math.PI / 2, 0]}
+          scale={1.04}
+        />
+        <group
+          name="cs_shoulder_right"
+          position={[0.5, 16.5, 0]}
+          rotation={[-Math.PI, -Math.PI / 2, 0]}
+          scale={1.04}
+        />
+        <group name="cs_sphere" position={[0.5, 2.5, 0]} scale={0.21} />
+        <group name="cs_sphere_012" position={[3.5, 2.5, 0]} scale={0.21} />
+        <group name="cs_square" position={[1.5, 1.5, 0]} rotation={[-Math.PI, 0, 0]} scale={0.15} />
+        <group
+          name="cs_square_2"
+          position={[0.5, 1.5, 0]}
+          rotation={[-Math.PI, 0, 0]}
+          scale={0.15}
+        />
+        <group name="cs_thigh_fk" position={[0.5, 7.5, 0]} scale={0.82} />
+        <group name="cs_toe" position={[0.5, 9.5, 0]} scale={0.43} />
+        <group name="Armature">
+          <primitive object={nodes.mixamorigHips} />
+          <primitive object={nodes.Ctrl_Master} />
+          <primitive object={nodes.Ctrl_ArmPole_IK_Left} />
+          <primitive object={nodes.Ctrl_Hand_IK_Left} />
+          <primitive object={nodes.Ctrl_ArmPole_IK_Right} />
+          <primitive object={nodes.Ctrl_Hand_IK_Right} />
+          <primitive object={nodes.Ctrl_Foot_IK_Left} />
+          <primitive object={nodes.Ctrl_LegPole_IK_Left} />
+          <primitive object={nodes.Ctrl_Foot_IK_Right} />
+          <primitive object={nodes.Ctrl_LegPole_IK_Right} />
+          <skinnedMesh
+            name="Man"
+            geometry={nodes.Man.geometry}
+            material={nodes.Man.material}
+            skeleton={nodes.Man.skeleton}
+          />
+        </group>
+        <group name="cs_grp">
+          <group name="cs_arm_fk001" position={[1.5, 8.5, 0]} scale={0.82} />
+          <group name="cs_calf_fk001" position={[0.5, 8.5, 0]} scale={0.82} />
+          <group name="cs_circle002" position={[0.5, 4.5, 0]} scale={0.21} />
+          <group name="cs_circle003" position={[0.5, 4.5, 0]} scale={0.21} />
+          <group name="cs_circle_025001" position={[2.5, 4.5, 0]} scale={0.21} />
+          <group
+            name="cs_foot003"
+            position={[0.5, 10.5, 0]}
+            rotation={[-Math.PI, 0, 0]}
+            scale={0.31}
+          />
+          <group
+            name="cs_foot004"
+            position={[0.5, 10.5, 0]}
+            rotation={[-Math.PI, 0, 0]}
+            scale={0.31}
+          />
+          <group
+            name="cs_foot005"
+            position={[0.5, 10.5, 0]}
+            rotation={[-Math.PI, 0, 0]}
+            scale={0.31}
+          />
+          <group
+            name="cs_foot_01001"
+            position={[0.5, 18.5, 0]}
+            rotation={[0, 1.57, 0]}
+            scale={2.19}
+          />
+          <group name="cs_foot_roll001" position={[0.5, 12.5, 0]} scale={0.59} />
+          <group name="cs_forearm_fk001" position={[2.5, 8.5, 0]} scale={0.82} />
+          <group
+            name="cs_hand001"
+            position={[0.5, 19.5, 0]}
+            rotation={[-Math.PI, 0, 0]}
+            scale={0.31}
+          />
+          <group name="cs_head001" position={[0.5, 13.5, 0]} scale={0.21} />
+          <group name="cs_hips001" position={[0.5, 11.5, 0]} scale={0.21} />
+          <group name="cs_master001" position={[0.5, 17.5, 0]} scale={0.1} />
+          <group name="cs_neck001" position={[0.5, 14.5, 0]} scale={0.21} />
+          <group
+            name="cs_shoulder_left001"
+            position={[0.5, 15.5, 0]}
+            rotation={[-Math.PI, -Math.PI / 2, 0]}
+            scale={1.04}
+          />
+          <group
+            name="cs_shoulder_right001"
+            position={[0.5, 16.5, 0]}
+            rotation={[-Math.PI, -Math.PI / 2, 0]}
+            scale={1.04}
+          />
+          <group name="cs_sphere001" position={[0.5, 2.5, 0]} scale={0.21} />
+          <group name="cs_sphere_012001" position={[3.5, 2.5, 0]} scale={0.21} />
+          <group
+            name="cs_square001"
+            position={[1.5, 1.5, 0]}
+            rotation={[-Math.PI, 0, 0]}
+            scale={0.15}
+          />
+          <group
+            name="cs_square_2001"
+            position={[0.5, 1.5, 0]}
+            rotation={[-Math.PI, 0, 0]}
+            scale={0.15}
+          />
+          <group name="cs_thigh_fk001" position={[0.5, 7.5, 0]} scale={0.82} />
+          <group name="cs_toe001" position={[0.5, 9.5, 0]} scale={0.43} />
+        </group>
       </group>
     </group>
   )
 }
 
-useGLTF.preload('./models/Man.glb')
+useGLTF.preload('./models/Man.gltf')
 
 export default Man
--- src/compontents/World.js
+++ src/compontents/World.js
@@ -14,7 +14,7 @@ const LEFT = new Vector3(-1, 0, 0)
 const BACK = new Vector3(0, 0, 1)
 
 const World = ({ width, height }) => {
-  const stageRef = useRef();
+  const stageRef = useRef()
   const [isZoom, setZoom] = useState(false)
   const [position, setPosition] = useState(new Vector3(0.5, 0.5, 0.5))
   // move the central box with the keyboard
@@ -62,24 +62,21 @@ const World = ({ width, height }) => {
     <div className="World" style={{ width, height, margin: 'auto', border: '1px solid' }}>
       {JSON.stringify(position)}
       <CharacterAnimationsProvider>
-      <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
-        {/* <OrthographicCamera makeDefault fov /> */}
-        <Suspense fallback={null}>
-        <Stage
-          controls={stageRef}
-          preset="rembrandt"
-          intensity={1}
-          environment="city"
-        ><Man scale={.01}/></Stage>
-        </Suspense>
-        {/* <gridHelper args={[16, 16, 'blue', 'gray']} />
+        <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+          {/* <OrthographicCamera makeDefault fov /> */}
+          <Suspense fallback={null}>
+            <Stage controls={stageRef} preset="rembrandt" intensity={1} environment="city">
+              <Man scale={0.01} />
+            </Stage>
+          </Suspense>
+          {/* <gridHelper args={[16, 16, 'blue', 'gray']} />
         <mesh onClick={() => setZoom(!isZoom)} position={position}>
           <boxGeometry attach="geometry" args={[1, 1, 1]} />
           <meshNormalMaterial attach="material" />
         </mesh> */}
-        {/* s<CameraDolly isZoom={isZoom} /> */}
-        <OrbitControls />
-      </Canvas>
+          {/* s<CameraDolly isZoom={isZoom} /> */}
+          <OrbitControls />
+        </Canvas>
       </CharacterAnimationsProvider>
       <WorldControls handleMove={handleMove} />
     </div>
