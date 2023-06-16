f946a119124b864cb183007ad88d9adefbd9b5cf
Daniele Guido <gui.daniele@gmail.com>
2023-06-16 15:55:01 +0200

    add css transformation to cards

--- src/components/ChapterCard.jsx
+++ src/components/ChapterCard.jsx
@@ -1,24 +1,64 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { a, useSpring } from '@react-spring/web'
 import './ChapterCard.css'
 import OverheadView from './Svg/OverheadView'
+import { useWindowStore } from '../store'
+
+const CardAspectRatio = 1.7777
 const ChapterCard = ({ chapter }) => {
-  const [flipped, set] = useState(false)
-  const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+  const ref = useRef(null)
+  const isFlipped = useRef(false)
+  const [width, height] = useWindowStore((state) => [state.width, state.height])
+
+  const [{ transform, opacity }, animate] = useSpring(() => ({
+    opacity: 0,
+    transform: `perspective(1000px) rotateY(0deg) scale(1)`,
     config: { mass: 5, tension: 500, friction: 80 },
-  })
+    onStart: () => {
+      if (isFlipped.current === true) {
+        ref.current.style.zIndex = 100
+      }
+    },
+    onRest: () => {
+      if (isFlipped.current === false) {
+        ref.current.style.zIndex = 0
+      }
+    },
+  }))
+  const hasCover = chapter.card?.srcset !== undefined
+  const clickHandler = () => {
+    // get ref box
+    console.log(isFlipped.current, ref.current.getBoundingClientRect())
+    // animate.start()
+    isFlipped.current = !isFlipped.current
+    if (isFlipped.current === true) {
+      const maxScale =
+        Math.min(width / ref.current.offsetWidth, height / ref.current.offsetHeight) * 0.9
+      animate.start({
+        opacity: 1,
+        transform: `perspective(1000px) rotateY(${180}deg) scale(${maxScale}) `,
+      })
+    } else {
+      animate.start({
+        opacity: 0,
+        transform: `perspective(1000px) rotateY(${0}deg) scale(${1}) `,
+      })
+    }
+  }
   return (
-    <div className="ChapterCard" onClick={() => set((state) => !state)}>
+    <div ref={ref} className="ChapterCard" onClick={clickHandler}>
       <a.div
         className="ChapterCard_front"
         style={{
           opacity: opacity.to((o) => 1 - o),
           transform,
+          perspective: '600px',
         }}
       >
         {chapter.title}
+        {hasCover && (
+          <img srcSet={chapter.card.srcset} alt={chapter.card.alt} className="ChapterCard_cover" />
+        )}
         <OverheadView />
       </a.div>
       <a.div
@@ -26,6 +66,7 @@ const ChapterCard = ({ chapter }) => {
         style={{
           opacity,
           transform,
+
           rotateX: '180deg',
         }}
       />
