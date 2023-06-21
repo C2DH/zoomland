cdde6c8950506acb41c2cd892010fb047aa7c411
Daniele Guido <gui.daniele@gmail.com>
2023-06-21 11:05:26 +0200

    add fullscreen

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,11 +1,34 @@
 import { Link } from 'react-router-dom'
 import { useWindowStore } from '../store'
+import { X } from '@phosphor-icons/react'
 import './Page.css'
 
-const Page = ({ status = 'idle', className = '', PageHeader = null, children }) => {
+const Page = ({
+  status = 'idle',
+  className = '',
+  PageHeader = null,
+  fullscreen = false,
+  children,
+}) => {
   const [width, height] = useWindowStore((state) => [state.width, state.height])
+  if (fullscreen) {
+    return (
+      <div className={`${className} Page scrollable page`} style={{ width, height }}>
+        {children}
+        <button
+          className="Page_close"
+          onClick={() => {
+            window.history.back()
+          }}
+        >
+          <X weight="regular" size={50} />
+        </button>
+      </div>
+    )
+  }
   return (
-    <div className={`${className} Page page d-flex flex-column`} style={{ width, height }}>
+    <div className={`${className} Page  page d-flex flex-column`} style={{ width, height }}>
+      {fullscreen && <div className="fullscreen"></div>}
       <header>
         <h2>
           <Link to="/">back</Link>
