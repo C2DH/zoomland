3acf426bd2aec5249b08b505923e31a575c884cc
Daniele Guido <gui.daniele@gmail.com>
2023-07-17 15:47:23 +0200

    Update Rig.jsx

--- src/components/Rig.jsx
+++ src/components/Rig.jsx
@@ -3,18 +3,20 @@ import { useThree } from '@react-three/fiber'
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Vector3 } from 'three'
-import { useMenuStore, usePlayerStore, useWorldStore } from '../store'
+import { Gameplay, useMenuStore, usePlayerStore, useWorldStore } from '../store'
 
 const Rig = () => {
   // function Rig({ position = new Vector3(0, 0, 2), focus = new Vector3(0, 0, 0) }) {
   // const { controls, scene } = useThree()
   const { pathname } = useLocation()
   const setCameraOffset = useWorldStore((state) => state.setCameraOffset)
+  const scene = usePlayerStore((state) => state.scene)
   const menuStatus = useMenuStore((state) => state.menuStatus)
   const isObservingLandscape = usePlayerStore((state) => state.isObservingLandscape)
   useEffect(() => {
     let radius = 2.5
     let elevation = 1.5
+    let followPlayer = scene === Gameplay
     if (isObservingLandscape) {
       radius = 5
       elevation = 3
@@ -27,8 +29,8 @@ const Rig = () => {
       radius = 5
       elevation = 3
     }
-    setCameraOffset({ radius, elevation })
-  }, [menuStatus, pathname, isObservingLandscape])
+    setCameraOffset({ radius, elevation, followPlayer })
+  }, [scene, menuStatus, pathname, isObservingLandscape])
   // return <CameraControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
   return null
 }
