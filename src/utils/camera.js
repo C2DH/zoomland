d8045d7d64768891006baaa90553ac961a6f1a72
Daniele Guido <gui.daniele@gmail.com>
2023-07-17 15:44:52 +0200

    Create camera.js

---
+++ src/utils/camera.js
@@ -0,0 +1,25 @@
+import { Vector3 } from 'three'
+
+function easeInOut(t) {
+  return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+}
+
+export const updateCamera = (
+  camera,
+  { radius = 2.5, elevation = 1.5, target, delta, angle, disable = false },
+) => {
+  if (disable) {
+    return
+  }
+  // the position should take into account the angle
+  const phi = -1 * (angle + Math.PI / 2)
+  camera.position.lerp(
+    new Vector3(
+      target.x + radius * Math.cos(phi),
+      target.y + elevation,
+      target.z + radius * Math.sin(phi),
+    ),
+    delta * 2,
+  )
+  camera.lookAt(new Vector3(target.x, target.y + 1.5, target.z))
+}
