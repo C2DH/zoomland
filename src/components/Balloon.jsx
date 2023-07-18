7b8590271603b2e7564c327d42083c36f21d1f74
Daniele Guido <gui.daniele@gmail.com>
2023-07-18 11:56:31 +0200

    balloon automatic waitingTime

--- src/components/Balloon.jsx
+++ src/components/Balloon.jsx
@@ -1,36 +1,40 @@
-import React, { useEffect, useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber'
+import React, { Suspense, useEffect, useRef } from 'react'
+import { Text, useGLTF } from '@react-three/drei'
 import { RigidBody } from '@react-three/rapier'
-import { usePlayerStore } from '../store'
 import { useSpring, easings } from '@react-spring/web'
 import Sensor from './Sensor'
 
-const UpWithPlayer = 'up-with-player'
-const DownWithPlayer = 'down-with-player'
-const MovingUpWithPlayer = 'moving-with-player'
+const GoingUpWithPlayer = 'up-with-player'
+const GroundWithPlayer = 'down-with-player'
+const MovingGoingUpWithPlayer = 'moving-with-player'
 
-const DownNoPlayer = 'down-no-player'
+const GroundNoPlayer = 'down-no-player'
 const MovingUpNoPlayer = 'moving-no-player'
 const UpNoPlayer = 'up-no-player'
-const MovingDownNoPlayer = 'moving-down-no-player'
+const MovingGroundNoPlayer = 'moving-down-no-player'
 
-const Balloon = (props) => {
+const CheckInText = 'Ready?'
+const CheckOutText = 'Free ride!'
+
+const Balloon = ({ yMax = 20, yMin = 0, waitingTime = 5000, ...props }) => {
   const collisionTimerRef = useRef()
   const collisionStatusRef = useRef(false)
   const intersectionStatusRef = useRef(false)
-
-  const baloonStatusRef = useRef(DownNoPlayer)
-
+  // text threejs reference and its countdown timer (if any)
+  const textRef = useRef()
+  const textTimerRef = useRef()
+  // balloon status, threejs reference and timer to go up and down
+  const baloonStatusRef = useRef(GroundNoPlayer)
   const balloonRef = useRef()
-  const bodyRef = useRef()
   const baloonTimerRef = useRef()
+  // rigidbody reference
+  const bodyRef = useRef()
   const { nodes, materials } = useGLTF('../assets/models/Balloon.glb')
 
   const [, api] = useSpring(() => ({
     n: 0,
     config: {
-      duration: 10000,
+      duration: 20000,
       easing: easings.easeInOutSine,
     },
     onChange: ({ value }) => {
@@ -43,78 +47,24 @@ const Balloon = (props) => {
       })
     },
     onRest: ({ value }) => {
-      console.log('[Baloon] arrived. onRest: ', value.n)
-      if (baloonStatusRef.current === UpWithPlayer) {
+      console.log(
+        '[Balloon] arrived. Altitude:',
+        value.n,
+        value.n > yMin ? 'up' : 'down',
+        baloonStatusRef.current,
+      )
+      if (value.n === yMax) {
+        baloonStatusRef.current =
+          intersectionStatusRef.current && collisionStatusRef.current ? UpWithPlayer : UpNoPlayer
+        textRef.current.text = 'Max altitude reached.'
+        console.log('[Balloon] should go down now...')
         landing()
+      } else if (value.n === yMin) {
+        baloonStatusRef.current = intersectionStatusRef.current ? GroundWithPlayer : GroundNoPlayer
+        takeOff()
       }
-      // if (value.n === 15) {
-      //   baloonStatusRef.current = UpNoPlayer
-      // } else if (value.n === 0) {
-      //   baloonStatusRef.current = DownNoPlayer
-      // }
     },
-    // onRest: ({ value }) => {
-    //   console.log('[Baloon] arrived. onRest: ', value.n)
-    //   baloonTimerRef.current = setTimeout(() => {
-    //     api.start({
-    //       n: 0,
-    //     })
-    //   }, 3000)
-
-    // balloonRef.current.position.y = n
-    // bodyRef.current.setNextKinematicTranslation({
-    //   x: balloonRef.current.position.x,
-    //   y: balloonRef.current.position.y,
-    //   z: balloonRef.current.position.z,
-    // })
-    // },
   }))
-
-  const checkBaloonStatus = () => {
-    console.log('[Balloon] check balloon status', baloonStatusRef.current)
-
-    if (baloonStatusRef.current === DownNoPlayer) {
-      baloonStatusRef.current = MovingUpNoPlayer
-      api.start({
-        n: 15,
-      })
-    } else if (baloonStatusRef.current === UpNoPlayer) {
-      baloonStatusRef.current = MovingDownNoPlayer
-      api.start({
-        n: 0,
-      })
-    }
-    console.log('[Balloon] update balloon status to:', baloonStatusRef.current)
-  }
-
-  useEffect(() => {
-    console.log('[Balloon] useEffect')
-    // check every 3 seconds if the balloon is still in the air
-    // baloonTimerRef.current = setInterval(checkBaloonStatus, 10000)
-    return () => {
-      clearTimeout(baloonTimerRef.current)
-      clearTimeout(collisionTimerRef.current)
-    }
-  }, [])
-
-  // useFrame((state, delta) => {
-  // const time = state.clock.getElapsedTime() / 10
-  // const positionFactor = Math.sin(time) * 10 + 10
-  // balloonRef.current.position.y = positionFactor * 2
-  // bodyRef.current.setNextKinematicTranslation({
-  //   x: balloonRef.current.position.x,
-  //   y: balloonRef.current.position.y,
-  //   z: balloonRef.current.position.z,
-  // })
-  // balloonRef.current.rotation.y += 0.001
-  // })
-
-  // const debounceCollisionExit = () => {
-  //   collisionStatusRef.current = false
-  //   clearTimeout(collisionTimerRef.current)
-  //   console.debug('[Balloon] collisionExitHandler')
-  //   observeLandscape(false)
-  // }
 
   const debounceCollisionEnter = () => {
     clearTimeout(collisionTimerRef.current)
@@ -135,25 +85,11 @@ const Balloon = (props) => {
     }, 2000)
   }
 
-  // const collisionEnterHandler = (e) => {
-  //   if (e.rigidBodyObject.name === 'player') {
-  //     if (collisionStatusRef.current === false) {
-  //       baloonStatusRef.current = MovingUpWithPlayer
-  //       console.log('[Balloon] collisionEnterHandler', e.rigidBodyObject.name)
-  //       observeLandscape(true)
-  //       clearTimeout(collisionTimerRef.current)
-  //     }
-  //     collisionStatusRef.current = true
-  //   }
-  // }
-
-  // const collisionExitHandler = (e) => {
-  //   if (e.rigidBodyObject.name === 'player') {
-  //     clearTimeout(collisionTimerRef.current)
-  //     collisionTimerRef.current = setTimeout(debounceCollisionExit, 2000)
-  //   }
-  // }
   const checkIn = () => {
+    if (baloonStatusRef.current !== GroundNoPlayer) {
+      // no checkin if the balloon is not on the ground
+      return
+    }
     console.debug(
       '[Balloon] checkIn',
       '\n - instersectionStatusRef.current:',
@@ -161,47 +97,87 @@ const Balloon = (props) => {
       '\n - collisionStatusRef.current:',
       collisionStatusRef.current,
     )
+    textRef.current.text = CheckInText
 
     if (intersectionStatusRef.current == true && collisionStatusRef.current == true) {
-      baloonStatusRef.current = UpWithPlayer
+      baloonStatusRef.current = GoingUpWithPlayer
       takeOff()
     }
-  }
-
-  const takeOff = () => {
-    console.debug('[Balloon] takeOff!')
-    // start the balloon
-    clearTimeout(baloonTimerRef.current)
-
-    baloonTimerRef.current = setTimeout(() => {
-      console.debug('[Balloon] fly!')
-      api.start({
-        n: 15,
-      })
-    }, 3000)
-  }
-
-  const landing = () => {
-    console.debug('[Balloon] landing...')
-    // start the balloon
-    clearTimeout(baloonTimerRef.current)
-
-    baloonTimerRef.current = setTimeout(() => {
-      console.debug('[Balloon] land!!')
-      api.start({
-        n: 0,
-      })
-    }, 3000)
   }
 
   const checkOut = () => {
     console.debug('[Balloon] checkOut...')
     // baloondemo
     if (!intersectionStatusRef.current) {
+      textRef.current.text = CheckOutText
       // demomode
-      baloonStatusRef.current = DownNoPlayer
+      baloonStatusRef.current = GroundNoPlayer
     }
   }
+
+  const takeOff = () => {
+    if (
+      baloonStatusRef.current !== GroundNoPlayer &&
+      baloonStatusRef.current !== GroundWithPlayer
+    ) {
+      console.warn('[Balloon] takeOff: not on the ground?')
+      landing()
+      return
+    }
+    console.debug('[Balloon] takeOff in 3s!')
+    countdown()
+    // start the balloon
+    clearTimeout(baloonTimerRef.current)
+
+    baloonTimerRef.current = setTimeout(() => {
+      console.debug('[Balloon] fly!')
+      api.start({
+        n: yMax,
+      })
+    }, waitingTime)
+  }
+
+  const landing = () => {
+    console.debug('[Balloon] landing...')
+    // start the balloon
+    clearTimeout(baloonTimerRef.current)
+    textRef.current.text = 'beware,  baloon is landing...'
+    baloonTimerRef.current = setTimeout(() => {
+      console.debug('[Balloon] land!!')
+      api.start({
+        n: yMin,
+      })
+    }, waitingTime)
+  }
+
+  const countdown = () => {
+    if (!textRef.current) {
+      return
+    }
+    console.debug('[Balloon] countdown...')
+    textRef.current.text = String(waitingTime / 1000)
+    clearInterval(textTimerRef.current)
+    textTimerRef.current = setInterval(() => {
+      if (textRef.current.text === '1') {
+        clearInterval(textTimerRef.current)
+        textRef.current.text = 'Fly!'
+        return
+      }
+      textRef.current.text = (parseInt(textRef.current.text) - 1).toString()
+    }, 1000)
+  }
+
+  useEffect(() => {
+    console.log('[Balloon] useEffect')
+    takeOff()
+    // check every 3 seconds if the balloon is still in the air
+    // baloonTimerRef.current = setInterval(checkBaloonStatus, 10000)
+    return () => {
+      clearTimeout(baloonTimerRef.current)
+      clearTimeout(collisionTimerRef.current)
+      clearInterval(textTimerRef.current)
+    }
+  }, [])
 
   return (
     <>
@@ -245,6 +221,20 @@ const Balloon = (props) => {
           <meshStandardMaterial color={'red'} depthWrite={false} opacity={0} transparent={true} />
         </mesh>
       </RigidBody>
+      <Suspense fallback={null}>
+        <group {...props} dispose={null}>
+          <Text
+            position={[1, 0.5, 0]}
+            ref={textRef}
+            scale={[0.1, 0.1, 0.1]}
+            color="black" // default
+            anchorX="center" // default
+            anchorY="middle" // default
+          >
+            {CheckOutText}
+          </Text>
+        </group>
+      </Suspense>
       <group {...props} dispose={null} ref={balloonRef}>
         <mesh
           castShadow
