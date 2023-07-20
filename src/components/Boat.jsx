1de323122f752da78c936bc3eae16645f5962ccb
Daniele Guido <gui.daniele@gmail.com>
2023-07-20 16:04:53 +0200

    Update Boat.jsx

--- src/components/Boat.jsx
+++ src/components/Boat.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import { OpenSea, usePlayerStore } from '../store'
+import { Gameplay, OpenSea, Start, usePlayerStore } from '../store'
 import { Vector3 } from 'three'
 import { updateCamera } from '../utils/camera'
+import { easings, useSpring } from '@react-spring/web'
 
 const Boat = ({
   positions = [
@@ -16,6 +17,19 @@ const Boat = ({
   const sceneRef = useRef(usePlayerStore.getState().scene)
   const { nodes, materials } = useGLTF('../assets/models/Boat.glb')
   const seed = Math.random() + 0.8
+  const [, api] = useSpring(() => ({
+    x: positions[0][0],
+    y: positions[0][1],
+    config: {
+      easing: easings.easeInOutSine,
+      duration: 10000,
+    },
+    onChange: ({ value }) => {
+      // console.debug('[Boat] onChange', value)
+      boatRef.current.position.x = value.x
+      boatRef.current.position.y = value.y
+    },
+  }))
 
   // Animate using nice springs instead
   useFrame((state, delta) => {
@@ -23,24 +37,45 @@ const Boat = ({
     const bendFactor = Math.sin(time * seed) * 0.2 + 0.2
     boatRef.current.rotation.z = bendFactor * 0.15
     boatRef.current.rotation.y = bendFactor * 0.1
-
-    // slowly moving the boat to the final position
-    // console.log(time)
-    boatRef.current.position.lerp(new Vector3(...positions[1]), delta * 0.04)
-    const target = boatRef.current.getWorldPosition(new Vector3())
-
     updateCamera(state.camera, {
-      target,
+      target: boatRef.current.getWorldPosition(new Vector3()),
       delta,
       angle: -Math.PI / 2,
       radius: 4,
       elevation: 2,
-      disable: sceneRef.current !== OpenSea,
+      disable: sceneRef.current === Gameplay,
     })
   })
+  // slowly moving the boat to the final position
+  // console.log(time)
+  //   boatRef.current.position.lerp(new Vector3(...positions[1]), delta * 0.04)
+
+  // })
 
   useEffect(() => {
-    return usePlayerStore.subscribe((state) => (sceneRef.current = state.scene))
+    return usePlayerStore.subscribe((state) => {
+      console.info('[Boat] scene changed', state.scene)
+      if (state.scene === OpenSea) {
+        // do a transition to the harbour
+        api.start({
+          x: positions[1][0],
+          y: positions[1][1],
+        })
+      } else if (state.scene === Start) {
+        // do a transition to the harbour
+        api.set({
+          x: positions[0][0],
+          y: positions[0][1],
+        })
+      } else {
+        // set the boat to the final position
+        api.set({
+          x: positions[1][0],
+          y: positions[1][1],
+        })
+      }
+      sceneRef.current = state.scene
+    })
   }, [])
 
   console.log('[Boat]', sceneRef.current)
