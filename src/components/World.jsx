7b54e227d693ed9f7b3a543d1a971c70d44d9e0c
donsiamese <donsiamese@gmail.com>
2023-08-03 09:13:59 +0200

    arrange objects properties
    16 chapters objects

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -128,35 +128,12 @@ const World = ({
             <Airship position={[6, 16, 0]} />
             <Pound />
             <Ocean scale={[12, 12, 12]} position={[0, -2.2, 0]} />
-            <Antenna rotation={[0, 0, 0]} scale={[2, 2, 2]} position={[-24.2, -1, -6.8]} />
-            <SwedishHorse rotation={[0, -2, 0]} scale={2.2} position={[69.81, 12.3, 45.32]} />
-            <Banjo
-              rotation={[0, 0, -0.1]}
-              scale={[0.2, 0.2, 0.2]}
-              position={[17.83, 10.68, 79.68]}
-            />
 
             <Kite rotation={[0, 0, 0]} scale={[1, 1, 1]} position={[2.06, -3.16, -30.67]} />
 
-            <FishNet rotation={[0, -2.2, 0]} scale={[2.5, 2.5, 2.5]} position={[2.2, -2.8, -30]} />
-            <StoneBig rotation={[0, 1, 0]} scale={[1.5, 1.5, 1.5]} position={[0.4, 14.2, -72.6]} />
-            <StoneBig rotation={[0, 0, 0]} scale={[3, 3, 3]} position={[-34.0, 0.3, 31.8]} />
-            <StoneBig rotation={[0, 0, 0]} scale={[2, 1.5, 2]} position={[4.8, 12.6, -58.4]} />
-            <StoneBig rotation={[1, 1.5, 1]} scale={[2.5, 1.5, 2]} position={[11.2, 12.5, -70.1]} />
-            <StoneBig rotation={[1, 1.5, 1]} scale={[2.5, 1.5, 2]} position={[-33.3, 0.8, 33.56]} />
-            <StoneBig rotation={[-0.5, 1.8, 1]} scale={[2.7, 2.9, 2.3]} position={[85, 7.2, 6]} />
-            <StoneBig
-              rotation={[-0.7, 1.9, 1.3]}
-              scale={[2.5, 2.5, 2]}
-              position={[16.45, 9.8, -50.67]}
-            />
-            <Theater rotation={[0, 0.2, 0]} scale={[3, 3, 3]} position={[-20, -0.7, 6]} />
-            <Transmitter scale={[0.2, 0.2, 0.2]} position={[-71.9, 9.45, 65.5]} />
             <Flag rotation={[0, 2.7, 0]} scale={[0.21, 0.3, 0.21]} position={[60.04, 11.7, 72.7]} />
             <Flag rotation={[0, 2.7, 0]} scale={[0.21, 0.3, 0.21]} position={[-75.3, 10.0, 2.2]} />
             <Flag rotation={[0, 0, 0]} scale={[0.21, 0.21, 0.21]} position={[39.7, 13.1, -52.5]} />
-            <ObservationTower scale={[0.1, 0.1, 0.1]} position={[-31.3, -0.3, 59.1]} />
-            <Windsock rotation={[0, 0, 0]} scale={0.4} position={[5.1, 8.8, 66]} />
 
             {/* //Pick color - Media #fa953b, History - #306fc5, Hermeneutics - #9e43c8, Digital landscapes - #ed407d, Intro - #7c7c7c */}
             <GroundViewSign
@@ -195,19 +172,6 @@ const World = ({
               position={[-13.1, -1, 64.4]}
               color={'#E7DF29'}
             />
-            <House
-              rotation={[0, 0 - 2.2, 0]}
-              scale={[5.5, 5.5, 5.5]}
-              position={[-32, -0.8, -18.4]}
-            />
-            <House rotation={[0, -2, 0]} scale={[5.5, 5.5, 5.5]} position={[-10, -0.8, 46]} />
-            <House
-              rotation={[0, 0 - 1.5, 0]}
-              scale={[5.5, 5.5, 5.5]}
-              position={[-12.0, -0.8, -24.4]}
-            />
-            {/*On the hill */}
-            <House rotation={[0, 1.5, 0]} scale={[5.5, 5.5, 5.5]} position={[45.2, 12.8, 56.68]} />
 
             <WindTurbine
               scale={[0.25, 0.25, 0.25]}
@@ -233,7 +197,7 @@ const World = ({
               rotation={[0, -0.9, 0]}
               rotationSpeed={2}
             />
-            {/*On the hill */}
+
             <WindTurbine
               scale={[0.25, 0.25, 0.25]}
               position={[92.85, 7.1, 29.54]}
@@ -241,37 +205,20 @@ const World = ({
               rotationSpeed={0.3}
             />
             <Physics debug={debugPhysics}>
-              <IcescreamTruck rotation={[0, 1.3, 0]} position={[-8.7, 0.1, 33.23]} scale={1.5} />
+              <Price {...Chapters[0]} />
 
-              <Boat
-                rotation={[0, 0, 0]}
-                scale={[2.5, 2.5, 2.5]}
-                positions={[
-                  [123.6, -1.6, -10.2],
-                  [91.6, -1.6, -10.2],
-                ]}
-              />
+              <Price {...Chapters[1]} />
 
-              <Player isMobile={isMobile} debug={debug} position={[94.88, 0.26, -14.2]}></Player>
-              <Landscape receiveShadow position={[0, -2, 0]} scale={10} debug={debug} />
-              <Forest positions={BigTrees.positions} scales={BigTrees.scales} maxScale={1.5}>
-                <FirTree />
-              </Forest>
-              <Forest positions={SmallTrees.positions} scales={SmallTrees.scales} maxScale={0.7}>
-                <FirTreeSmall />
-              </Forest>
               <Balloon
                 yMax={30}
                 rotation={[0, 0, 0]}
                 scale={[3, 3, 3]}
-                position={[-24.3, 0.03, 47.6]}
+                position={Chapters[2].position}
               />
-              <Price {...Chapters[0]} />
-              <Price {...Chapters[1]} />
 
               <Target
                 chapter={Chapters[3]}
-                position={[-60, 27, -58]}
+                position={Chapters[3].position}
                 geometry="cylinder"
                 geometryArgs={[3.2, 3.2, 5, 8]}
                 additionalGeometry="box"
@@ -283,20 +230,87 @@ const World = ({
               >
                 <Windmill scale={0.3} rotation={[0, 1, 0]} />
               </Target>
+
+              <Target
+                chapter={Chapters[6]}
+                position={Chapters[6].position}
+                childrenIsRigidBody
+                transparent
+                height={1}
+                priceOffsetPosition={[2, -1, 2]}
+              >
+                <Antenna rotation={[0, 0, 0]} scale={[2, 2, 2]} />
+              </Target>
+
+              <Target
+                chapter={Chapters[7]}
+                position={Chapters[7].position}
+                geometryArgs={[0.9, 0.9, 5, 8]}
+                transparent
+                height={1}
+                priceOffsetPosition={[2, -1, 2]}
+              >
+                <Transmitter scale={[0.2, 0.2, 0.2]} />
+              </Target>
+
+              <Target
+                chapter={Chapters[8]}
+                position={Chapters[8].position}
+                geometry="box"
+                geometryArgs={[1.4, 6, 0.6]}
+                geometryPosition={[0.4, 0, -1]}
+                geometryRotation={[0, -0.45, 0]}
+                additionalGeometry="box"
+                additionalGeometryArgs={[1.4, 6, 0.8]}
+                additionalGeometryOffsetPosition={[-1.0, 0, 2.1]}
+                additionalGeometryRotation={[0, -0.5, 0]}
+                transparent
+                height={1}
+                priceOffsetPosition={[2, -1, 2]}
+              >
+                <SwedishHorse rotation={[0, -2, 0]} scale={2.2} />
+              </Target>
+
               <Target
                 chapter={Chapters[9]}
                 position={Chapters[9].position}
+                geometry="box"
+                geometryArgs={[1.8, 6, 1.8]}
+                geometryPosition={[-1, 0, -2.7]}
+                geometryRotation={[0, 0.3, 0]}
+                additionalGeometry="box"
+                additionalGeometryArgs={[2, 6, 2.2]}
+                additionalGeometryRotation={[0, 0.2, 0]}
+                additionalGeometryOffsetPosition={[0.5, 0, 2.2]}
                 transparent
-                childrenIsRigidBody
                 height={1}
                 priceOffsetPosition={[2, -1, 2]}
               >
                 <Arch rotation={[0, 0.3, 0]} scale={[2.4, 2.4, 2.4]} />
               </Target>
-              {/* chapter 15 */}
-              <Lighthouse scale={5} position={[54, 20.4, -85]} />
-              <Price {...Chapters[15]} />
-              {/* end of chqpter 15 */}
+
+              <Target
+                chapter={Chapters[10]}
+                position={Chapters[10].position}
+                geometry="box"
+                geometryArgs={[14, 3.2, 8]}
+                geometryPosition={[0, 0, 0]}
+                geometryRotation={[0, 0.2, 0]}
+                priceOffsetPosition={[0, 0, 5.22]}
+                transparent
+              >
+                <Theater rotation={[0, 0.2, 0]} scale={[3, 3, 3]} />
+              </Target>
+
+              <Target
+                chapter={Chapters[11]}
+                position={Chapters[11].position}
+                geometryArgs={[2, 2, 3, 8]}
+                transparent
+              >
+                <BigTree rotation={[0, -2, 0]} scale={[4, 4, 4]} />
+              </Target>
+
               <Target
                 chapter={Chapters[12]}
                 position={Chapters[12].position}
@@ -307,23 +321,121 @@ const World = ({
                 priceElevation={1}
                 PriceComponent={GroundViewSign}
                 color="blue"
-              />
-              <Megaphone
-                rotation={[0, 0, 0.3]}
-                scale={[0.2, 0.2, 0.2]}
-                position={Chapters[12].position}
-              />
-              <Target
-                chapter={Chapters[11]}
-                position={Chapters[11].position}
-                geometryArgs={[2, 2, 3, 8]}
-                transparent
               >
-                <BigTree rotation={[0, -2, 0]} scale={[4, 4, 4]} />
+                <Megaphone rotation={[0, 0, 0.3]} scale={[0.2, 0.2, 0.2]} />
               </Target>
+
+              <Target
+                chapter={Chapters[13]}
+                position={Chapters[13].position}
+                transparent
+                height={2.5}
+                priceOffsetPosition={[0.5, -1.25, 0]}
+                priceElevation={1}
+                PriceComponent={GroundViewSign}
+                color="blue"
+              >
+                <FishNet rotation={[0, -2.2, 0]} scale={[2.5, 2.5, 2.5]} />
+              </Target>
+
               <Target chapter={Chapters[14]} position={Chapters[14].position}>
                 <Ufo scale={[0.4, 0.4, 0.4]} />
               </Target>
+
+              <Price {...Chapters[15]} />
+              <Target
+                chapter={Chapters[15]}
+                position={Chapters[15].position}
+                geometryArgs={[2.6, 2.36, 5, 8]}
+                transparent
+                height={1}
+                priceOffsetPosition={[2, -1, 2]}
+              >
+                <Lighthouse scale={5} />
+              </Target>
+
+              <Target
+                chapter={Chapters[16]}
+                position={Chapters[16].position}
+                geometry="box"
+                geometryArgs={[0.6, 6, 0.6]}
+                geometryPosition={[0, 0, 0]}
+                geometryRotation={[0, 0, 0]}
+                transparent
+                height={1}
+                priceOffsetPosition={[2, -1, 2]}
+              >
+                <Windsock rotation={[0, 0, 0]} scale={0.4} />
+              </Target>
+
+              <IcescreamTruck rotation={[0, 1.3, 0]} scale={1.5} position={[-8.7, 0.03, 33.23]} />
+
+              <ObservationTower scale={[0.1, 0.1, 0.1]} position={[-29.01, 0.07, 53.74]} />
+              <Boat
+                rotation={[0, 0, 0]}
+                scale={[2.5, 2.5, 2.5]}
+                positions={[
+                  [123.6, -1.6, -10.2],
+                  [91.6, -1.6, -10.2],
+                ]}
+              />
+
+              <Landscape receiveShadow position={[0, -2, 0]} scale={10} debug={debug} />
+              <Forest positions={BigTrees.positions} scales={BigTrees.scales} maxScale={1.5}>
+                <FirTree />
+              </Forest>
+              <Forest positions={SmallTrees.positions} scales={SmallTrees.scales} maxScale={0.7}>
+                <FirTreeSmall />
+              </Forest>
+              {/*House - next to Hill*/}
+              <House
+                rotation={[0, 1.5, 0]}
+                scale={[5.5, 5.5, 5.5]}
+                position={[45.2, 12.8, 56.68]}
+              />
+
+              {/*House - next to Beach*/}
+              <House rotation={[0, -2, 0]} scale={[5.5, 5.5, 5.5]} position={[-10, -0.8, 46]} />
+
+              {/*House - next to Theatre*/}
+              <House
+                rotation={[0, 0 - 2.2, 0]}
+                scale={[5.5, 5.5, 5.5]}
+                position={[-32, -0.8, -18.4]}
+              />
+
+              {/*House - next to Theatre*/}
+              <House
+                rotation={[0, 0 - 1.5, 0]}
+                scale={[5.5, 5.5, 5.5]}
+                position={[-12.0, -0.8, -24.4]}
+              />
+
+              <StoneBig
+                rotation={[0, 1, 0]}
+                scale={[1.5, 1.5, 1.5]}
+                position={[0.4, 14.2, -72.6]}
+              />
+              <StoneBig rotation={[0, 0, 0]} scale={[3, 3, 3]} position={[-34.0, 0.3, 31.8]} />
+              <StoneBig rotation={[0, 0, 0]} scale={[2, 1.5, 2]} position={[4.8, 12.6, -58.4]} />
+              <StoneBig
+                rotation={[1, 1.5, 1]}
+                scale={[2.5, 1.5, 2]}
+                position={[11.2, 12.5, -70.1]}
+              />
+              <StoneBig
+                rotation={[1, 1.5, 1]}
+                scale={[2.5, 1.5, 2]}
+                position={[-33.3, 0.8, 33.56]}
+              />
+              <StoneBig rotation={[-0.5, 1.8, 1]} scale={[2.7, 2.9, 2.3]} position={[85, 7.2, 6]} />
+              <StoneBig
+                rotation={[-0.7, 1.9, 1.3]}
+                scale={[2.5, 2.5, 2]}
+                position={[16.45, 9.8, -50.67]}
+              />
+
+              <Player isMobile={isMobile} debug={debug} position={[94.88, 0.26, -14.2]}></Player>
 
               <TheDispatcher quest={Quests[5]} position={[46.87, 21.06, -81.69]}>
                 <Character scale={0.1} rotation={[0, 0, 0]} />
