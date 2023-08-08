a1b34169c64527d318ca2981231079060f62ea59
donsiamese <donsiamese@gmail.com>
2023-08-08 09:05:25 +0200

    change color code from vars to #

--- src/constants.js
+++ src/constants.js
@@ -8,11 +8,11 @@ export const CategoryMedia = 'media'
 export const CategoryHermeneutics = 'hermeneutics'
 export const CategoryDigitalLandscapes = 'digital landscapes'
 
-export const ColorIntroduction = 'red'
-export const ColorHistory = 'var(--history)'
-export const ColorMedia = 'var(--orange)'
-export const ColorHermeneutics = 'var(--purple)'
-export const ColorDigitalLandscapes = 'var(--rose)'
+export const ColorIntroduction = '#7c7c7c'
+export const ColorHistory = '#306fc5'
+export const ColorMedia = '#fa953b'
+export const ColorHermeneutics = '#9e43c8'
+export const ColorDigitalLandscapes = '#ed407d'
 
 export const ColorByCategory = {
   [CategoryIntroduction]: ColorIntroduction,
