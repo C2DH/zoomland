a716ebe5816628d847523a5828347e6bf10e5a2c
donsiamese <donsiamese@gmail.com>
2023-08-09 10:41:26 +0200

    add game controls

--- src/components/GameControls.jsx
+++ src/components/GameControls.jsx
@@ -2,6 +2,9 @@ import { useSpring, a } from '@react-spring/web'
 import { MenuClosed, useMenuStore } from '../store'
 import './GameControls.css'
 import { useEffect } from 'react'
+import RoundButton from './RoundButton'
+import CloseIcon from './Svg/CloseIcon'
+import { isMobile } from 'react-device-detect'
 
 const GameControls = () => {
   const [gameControlsStatus, setGameControlsStatus] = useMenuStore((state) => [
@@ -20,15 +23,35 @@ const GameControls = () => {
   useEffect(() => {
     api.start({
       opacity: gameControlsStatus === MenuClosed ? 0 : 1,
+      display: gameControlsStatus === MenuClosed ? 'none' : 'block',
     })
   }, [gameControlsStatus])
 
   return (
-    <a.div style={style} className="GameControls p-5">
-      GameControls
-      <button className="btn btn-link btn-lg" onClick={hide}>
-        Got it!
-      </button>
+    <a.div style={style} className="GameControls">
+      <div style={{ marginRight: '0.7rem', position: 'absolute', right: 0, top: '0.7rem' }}>
+        <RoundButton
+          Icon={CloseIcon}
+          color={'var(--pale-yellow)'}
+          backgroundColor={'var(--transparent)'}
+          onClick={hide}
+        />
+      </div>
+      <h2 className="mb-4">Game Controls</h2>
+      {isMobile ? (
+        <img
+          src="/assets/svg/character-controls-mobile.svg"
+          alt="Game controls"
+          className="GameControls-img w-100"
+        />
+      ) : (
+        <img
+          src="/assets/svg/character-controls-desktop.svg"
+          alt="Game controls"
+          className="GameControls-img w-100"
+        />
+      )}
+      <div className="background-pattern" style={{ width: '100vw', zIndex: '-1' }}></div>
     </a.div>
   )
 }
