46bb8597cf339c1414ebdaae20bc51cf96fd8532
donsiamese <donsiamese@gmail.com>
2023-08-23 16:37:25 +0200

    shadows positon fix

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -7,7 +7,9 @@ const Dawn = 0xffe6b3
 const Lights = ({ color = Dawn }) => {
   const light = useRef()
   useFrame((state) => {
-    light.current.position.z = state.camera.position.z + 1
+    light.current.position.z = state.camera.position.z
+    light.current.position.x = state.camera.position.x + 20
+    light.current.position.y = state.camera.position.y + 60
   })
   return (
     <>
@@ -16,18 +18,18 @@ const Lights = ({ color = Dawn }) => {
         intensity={1}
         castShadow
         color={color}
-        position={[0, 80, 80]}
+        // position={[40, 10, 25]}
         shadow-mapSize={[2048, 2048]}
         shadow-camera-near={20}
-        shadow-camera-far={100}
-        shadow-camera-top={100}
-        shadow-camera-right={100}
-        shadow-camera-bottom={-100}
-        shadow-camera-left={-100}
+        shadow-camera-far={150}
+        shadow-camera-top={150}
+        shadow-camera-right={150}
+        shadow-camera-bottom={-150}
+        shadow-camera-left={-150}
         shadow-bias={-0.01}
       />
 
-      <ambientLight intensity={0.1} />
+      <ambientLight intensity={0.12} />
     </>
   )
 }
