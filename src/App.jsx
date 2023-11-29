93c5108d57a532f6c27e6add7b9b0a0f8f765e5c
donsiamese <donsiamese@gmail.com>
2023-11-29 14:56:20 +0100

    game controls - shows after introdution

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useCallback, useEffect } from 'react'
+import React, { Suspense, useCallback, useEffect, useState } from 'react'
 import Header from './components/Header'
 import Vignette from './components/Vignette'
 import World from './components/World'
@@ -18,12 +18,34 @@ import GlitchingBooks from './components/GlitchingBooks'
 import Endings from './components/Endings'
 import RoundButton from './components/RoundButton'
 import MapIcon from './components/Svg/MapIcon'
+import { Gameplay, usePlayerStore } from './store/index.js'
+import { MenuClosed, MenuOpen, useMenuStore } from './store'
 
 const MapPage = React.lazy(() => import('./pages/Map'))
 const ChaptersPage = React.lazy(() => import('./pages/Chapters'))
 const queryClient = new QueryClient()
 
 function App() {
+  const [active, setActive] = useState(true)
+  const setGameControlsStatus = useMenuStore((state) => state.setGameControlsStatus)
+  const scene = usePlayerStore.getState().scene
+
+  const deactivateMapBtn = () => {
+    setActive(false)
+  }
+
+  useEffect(() => {
+    setGameControlsStatus(MenuOpen)
+    if (scene === Gameplay) {
+      const timeoutId = setTimeout(() => {
+        setGameControlsStatus(MenuClosed)
+      }, 10000)
+
+      // Cleanup the timeout to avoid memory leaks
+      return () => clearTimeout(timeoutId)
+    }
+  }, []) // Empty dependency array ensures that this effect runs only
+
   const updateDimensions = useWindowStore((state) => state.updateDimensions)
   const change = () => {
     console.debug('[app] change')
@@ -57,7 +79,12 @@ function App() {
       <GlitchingBooks />
       <div className="mapButton fill position-fixed bottom-0">
         <Link to="/map">
-          <RoundButton Icon={MapIcon}></RoundButton>
+          <RoundButton
+            onClick={deactivateMapBtn}
+            active={active}
+            Icon={MapIcon}
+            margin={isMobile ? '1rem' : '2rem'}
+          ></RoundButton>
         </Link>
       </div>
       <SideMenu />
