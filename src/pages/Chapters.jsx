465b7ac8c4780a13ef8399b09354b92131424a48
Daniele Guido <gui.daniele@gmail.com>
2023-12-03 22:57:15 +0100

    add OnFlipped listener for cards

--- src/pages/Chapters.jsx
+++ src/pages/Chapters.jsx
@@ -3,23 +3,12 @@ import Page from './Page'
 import ChapterCard from '../components/ChapterCard'
 import { usePlayerStore } from '../store'
 import data from '../data/chapters.json'
+// import Quest from '../components/Quest'
 
 const Chapters = () => {
   const [collectedChapters] = usePlayerStore((state) => [state.collectedChapters])
 
-  console.debug('[Chapters] collectedChapters', collectedChapters)
-  // load chapter using useQuery
-  // const { status, error, data } = useQuery({
-  //   queryKey: ['chapters'],
-  //   queryFn: ({ signal }) => {
-  //     return axios
-  //       .get('/data/chapters.json', {
-  //         signal,
-  //       })
-  //       .then(({ data }) => data)
-  //   },
-  // })
-
+  const onFlippedHandler = (isFlipped) => console.debug('[Chapters] onFlipped', isFlipped)
   return (
     <Page fullscreen className="p-5" PageHeader={() => <h1> Chapters</h1>}>
       <Container>
@@ -28,6 +17,7 @@ const Chapters = () => {
             <Col key={chapter.id} xs={12} md={6} lg={4} xl={3} xxl={3} className="mb-4">
               <ChapterCard
                 chapter={chapter}
+                onFlipped={onFlippedHandler}
                 notFound={!collectedChapters.some((d) => d.id === chapter.id)}
               />
               {/* <ChapterListItem chapter={chapter} /> */}
