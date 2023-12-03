e68d996e2cfe7f393fd9db77716a7b9b64c95bb6
Daniele Guido <gui.daniele@gmail.com>
2023-12-03 22:00:33 +0100

    remove button of skiintro

--- src/components/SkipIntro.jsx
+++ src/components/SkipIntro.jsx
@@ -44,34 +44,14 @@ const SkipIntro = () => {
 
   return (
     <a.div style={style} className="SkipIntro">
-      {scene === Start && isLoadingComplete ? (
+      {scene === Start && isLoadingComplete && (
         <Quest
           initialDelay={100}
           quest={IntroQuest}
           onComplete={onIntroQuestComplete}
           onCompleteLabel="Let's get to the harbour!"
         />
-      ) : isLoadingComplete ? (
-        <>
-          <RoundButton
-            color={'var(--pale-yellow)'}
-            onClick={() => setScene(scene === Start ? OpenSea : Gameplay)}
-            text={
-              (scene === Start && "Let's get to the harbour!") ||
-              (scene === OpenSea && "Let's get to the island!") ||
-              scene
-            }
-          />
-          {/* <button
-            className="btn btn-block btn-primary btn-lg"
-            onClick={() => setScene(scene === Start ? OpenSea : Gameplay)}
-          >
-            {scene === Start && <span>Let's get to the harbour!</span>}
-            {scene === OpenSea && <span>Let's get to the island!</span>}
-            {scene}
-          </button> */}
-        </>
-      ) : null}
+      )}
     </a.div>
   )
 }
