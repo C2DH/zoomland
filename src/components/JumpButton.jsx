b40862bc0cb2f099d1fb9ca54c70c12a7c6de9af
donsiamese <donsiamese@gmail.com>
2023-12-19 17:02:33 +0100

    jump button css fix

--- src/components/JumpButton.jsx
+++ src/components/JumpButton.jsx
@@ -1,16 +1,20 @@
 import './JumpButton.css'
 import { useSpring, a, config } from '@react-spring/web'
 
-const JumpButton = ({ onClick, className='' }) => {
+const JumpButton = ({ onClick, className = '' }) => {
   const [style, api] = useSpring(() => ({ scale: 0, config: config.gentle }))
 
   const handleClick = (e) => {
-    api.start({ scale: 3, config: config.gentle, onRest: () => api.start({ scale: 0, config: config.stiff  }) })
+    api.start({
+      scale: 2.5,
+      config: config.gentle,
+      onRest: () => api.start({ scale: 0, config: config.stiff }),
+    })
     onClick(e)
   }
 
   const handleTouchEnd = () => {
-    api.start({ scale: 0,  config: config.stiff })
+    api.start({ scale: 0, config: config.stiff })
   }
 
   return (
